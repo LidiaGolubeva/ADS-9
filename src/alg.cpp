abd725309c5a21ee0060257af9700e8c1b71c7bc
LidiaGolubeva <125212839+LidiaGolubeva@users.noreply.github.com>
2023-06-04 23:16:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,23 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+  if (!file.is_open()) {
+    throw std::string("File error!");
+  }
+  std::string word = "";
+  BST<std::string> bst;
+  while (!file.eof()) {
+    word = "";
+    while (1) {
+      char character = file.get();
+      if ((character >= 65 && character <= 90) || (character >= 97 && character <= 122)) {
+        word += tolower(character);
+      } else {
+        break;
+      }
+    }
+    bst.AddValue(word);
+  }
+  return bst;
 }
